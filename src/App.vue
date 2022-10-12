b3e6394d4d861569e3b884a11d032519486612d8
jenniferzusukicruzvillagomez1 <108487642+jenniferzusukicruzvillagomez1@users.noreply.github.com>
2022-10-12 18:06:52 -0500

    segunda edicion

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mt-3">
+    <Header></Header>
+  <Formulario></Formulario>
+
+      
+</div>
+</template>
+
+<script>
+  import Header from "./components/Header.vue";
+  import Formulario from "./components/Formulario.vue";
+  export default{
+    components:{
+    Header,
+    Formulario
+  },
+  };
+
+</script>
+<style>
+
+</style>
